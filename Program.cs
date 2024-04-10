5ee37e6a21f8fa7f25ffa2992d17fc90d373b4e8
Ahmed ms <164795778+Odiskalilnx9213@users.noreply.github.com>
2024-04-10 07:14:50 +0200

    Add files via upload
    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+   static void Main(string[] args)
+   {
+      Console.WriteLine("Enter the first limit:");
+      int limit1 = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("Enter the second limit:");
+      int limit2 = Convert.ToInt32(Console.ReadLine());
+
+      // Ensure limit1 is the minimum and limit2 is the maximum
+      int min = Math.Min(limit1, limit2);
+      int max = Math.Max(limit1, limit2);
+
+      Console.WriteLine("Enter an integer to limit:");
+      int number = Convert.ToInt32(Console.ReadLine());
+
+      if (number > max)
+      {
+         Console.WriteLine($"You have entered {number} which is greater than {max} which is the maximum");
+         number = max;
+      }
+      else if (number < min)
+      {
+         Console.WriteLine($"You have entered {number} which is lower than {min} which is the minimum");
+         number = min;
+      }
+
+      Console.WriteLine($"Resulting integer: {number}");
+   }
+}
+
